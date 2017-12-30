269a38f8b031cc43025c57641cdc178092cd4777
Kaloyan97 <33879929+Kaloyan97@users.noreply.github.com>
2017-12-30 19:29:31 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.blue = new System.Windows.Forms.PictureBox();
+            this.green = new System.Windows.Forms.PictureBox();
+            this.red = new System.Windows.Forms.PictureBox();
+            this.default1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.blue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.green)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.red)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.default1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.blue);
+            this.panel1.Controls.Add(this.green);
+            this.panel1.Controls.Add(this.red);
+            this.panel1.Controls.Add(this.default1);
+            this.panel1.Location = new System.Drawing.Point(12, 533);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(513, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // blue
+            // 
+            this.blue.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.blue.Location = new System.Drawing.Point(321, 30);
+            this.blue.Name = "blue";
+            this.blue.Size = new System.Drawing.Size(100, 50);
+            this.blue.TabIndex = 3;
+            this.blue.TabStop = false;
+            this.blue.Click += new System.EventHandler(this.blue_Click);
+            // 
+            // green
+            // 
+            this.green.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.green.Location = new System.Drawing.Point(215, 30);
+            this.green.Name = "green";
+            this.green.Size = new System.Drawing.Size(100, 50);
+            this.green.TabIndex = 2;
+            this.green.TabStop = false;
+            this.green.Click += new System.EventHandler(this.green_Click);
+            // 
+            // red
+            // 
+            this.red.BackColor = System.Drawing.Color.Red;
+            this.red.Location = new System.Drawing.Point(109, 30);
+            this.red.Name = "red";
+            this.red.Size = new System.Drawing.Size(100, 50);
+            this.red.TabIndex = 1;
+            this.red.TabStop = false;
+            this.red.Click += new System.EventHandler(this.red_Click);
+            // 
+            // default1
+            // 
+            this.default1.BackColor = System.Drawing.Color.Black;
+            this.default1.Location = new System.Drawing.Point(3, 30);
+            this.default1.Name = "default1";
+            this.default1.Size = new System.Drawing.Size(100, 50);
+            this.default1.TabIndex = 0;
+            this.default1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1467, 645);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.blue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.green)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.red)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.default1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox blue;
+        private System.Windows.Forms.PictureBox green;
+        private System.Windows.Forms.PictureBox red;
+        private System.Windows.Forms.PictureBox default1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Pen p=new Pen(Color.Black, 1);
+        Point sp = new Point(0, 0);
+        Point ep = new Point(0, 0);
+        int k = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void red_Click(object sender, EventArgs e)
+        {
+            p.Color = red.BackColor;
+            default1.BackColor = red.BackColor;
+        }
+
+        private void green_Click(object sender, EventArgs e)
+        {
+            p.Color = green.BackColor;
+            default1.BackColor = green.BackColor;
+        }
+
+        private void blue_Click(object sender, EventArgs e)
+        {
+            p.Color = blue.BackColor;
+            default1.BackColor = blue.BackColor;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            sp = e.Location;
+            if (e.Button == MouseButtons.Left)
+                k = 1;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            k = 0;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (k==1)
+            {
+                ep = e.Location;
+                g = this.CreateGraphics();
+                g.DrawLine(p, sp, ep);
+            }
+            sp = ep;
+        }
+    }
+}
